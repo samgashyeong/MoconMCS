19f33f85f4f5ab7f68b0c21f7cec88335671f659
samgashyeong <samgashyeong@naver.com>
2021-05-17 18:19:28 +0900

    hi1

--- app/src/main/java/com/example/moconmcs/MainActivity.kt
+++ app/src/main/java/com/example/moconmcs/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val  a = 1
+        val b= 2
+        var c = a+b
+
     }
 }
