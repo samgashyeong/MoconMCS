5f6799c6d40e2079f758f79504e3152ad2d8111f
whdbcks111 <whdbcks111@gmail.com>
2021-05-17 18:24:28 +0900

    hi

--- app/src/main/java/com/example/moconmcs/MainActivity2.java
+++ app/src/main/java/com/example/moconmcs/MainActivity2.java
@@ -3,6 +3,8 @@ package com.example.moconmcs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -12,5 +14,9 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
         int a = 1;
         String jun = "sang";
+        Button btn1 = findViewById(R.id.button2);
+        btn1.setOnClickListener(v -> {
+            v.setBackgroundColor(0xff0000);
+        });
     }
 }
