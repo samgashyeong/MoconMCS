c0a5c4efef4ce0bffccddbd7530275e68fb628a1
whdbcks111 <whdbcks111@gmail.com>
2021-06-11 01:03:34 +0900

    온보딩 프래그먼트 작동 방식 수정

--- app/src/main/java/com/example/moconmcs/Onboarding/OnboardingActivity.java
+++ app/src/main/java/com/example/moconmcs/Onboarding/OnboardingActivity.java
@@ -9,9 +9,11 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.moconmcs.LoginActivity;
 import com.example.moconmcs.Main.MainActivity;
 import com.example.moconmcs.R;
 import com.google.firebase.auth.FirebaseAuth;
@@ -22,26 +24,20 @@ import java.util.List;
 
 public class OnboardingActivity extends AppCompatActivity {
 
-    private Button next, prev;
-    
+    private float x1, x2;
+    private static final float MIN_SWIPE_DISTANCE = 150;
+
+    private Button success;
     private int curPage = 0;
     private final List<Class<? extends Fragment>> pageFragments = Arrays.asList(
             OnboardingFragment1.class,
             OnboardingFragment2.class,
             OnboardingFragment3.class,
-            OnboardingFragment4.class,
-            LoginFragment.class
+            OnboardingFragment4.class
     );
     private FirebaseAuth auth;
 
     private void switchFragment() {
-
-        if(prev == null || next == null) return;
-        
-        if(curPage == 0) prev.setVisibility(View.GONE);
-        else prev.setVisibility(View.VISIBLE);
-        if(curPage == pageFragments.size() - 1) next.setVisibility(View.GONE);
-        else next.setVisibility(View.VISIBLE);
 
         Fragment fr = null;
         Class<? extends Fragment> frclass = pageFragments.get(curPage);
@@ -61,8 +57,30 @@ public class OnboardingActivity extends AppCompatActivity {
         fragmentTransaction.commit();
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        switch(event.getActionMasked())
+        {
+            case MotionEvent.ACTION_DOWN:
+                x1 = event.getX();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = event.getX();
+                float deltaX = x2 - x1;
+                if (deltaX > MIN_SWIPE_DISTANCE)
+                {
+                    prevPage();
+                }
+                else if (deltaX < -MIN_SWIPE_DISTANCE)
+                {
+                    nextPage();
+                }
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
 
-    @SuppressLint("WrongConstant")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -70,28 +88,29 @@ public class OnboardingActivity extends AppCompatActivity {
 
         auth = FirebaseAuth.getInstance();
 
-        Intent intent = getIntent();
-
         if(auth.getCurrentUser() != null){
             startActivity(new Intent(this, MainActivity.class));
             finish();
         }
 
-        next = findViewById(R.id.obnext);
-        prev = findViewById(R.id.obprev);
-        
-        if(curPage == 0) prev.setVisibility(View.GONE);
-        if(curPage == 3) next.setText("완료!");
+        success = findViewById(R.id.obsuccess);
+        success.setOnClickListener(v -> nextPage());
+
+
+        if(curPage == pageFragments.size() - 1) {
+            success.setVisibility(View.VISIBLE);
+            success.setActivated(true);
+            success.setText("완료!");
+        }
+        else {
+            success.setVisibility(View.INVISIBLE);
+            success.setActivated(false);
+        }
 
         FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fm.beginTransaction();
 
-        if(intent.getBooleanExtra("isLoginBack", false)) {
-            curPage = pageFragments.size() - 1;
-            fragmentTransaction.add(R.id.obframe, new LoginFragment());
-            switchFragment();
-        }
-        else fragmentTransaction.add(R.id.obframe, new OnboardingFragment1());
+        fragmentTransaction.add(R.id.obframe, new OnboardingFragment1());
 
 //        SharedPreferences preferences = getSharedPreferences("FirstCheck", Activity.MODE_PRIVATE);
 //        boolean checkFirst = preferences.getBoolean("checkFirst", false);
@@ -108,23 +127,22 @@ public class OnboardingActivity extends AppCompatActivity {
 //            finish();
 //        }
 
-        next.setOnClickListener(v -> {
-            curPage++;
-            if(curPage > 4) {
-                startActivity(new Intent(OnboardingActivity.this, MainActivity.class)
-                .setFlags(Intent.EXTRA_DOCK_STATE_UNDOCKED));
-                Intent intent1 = new Intent();
-                intent1.setFlags(Intent.EXTRA_DOCK_STATE_HE_DESK);
-                finish();
-            }
-            else switchFragment();
-        });
-        prev.setOnClickListener(v -> {
-            curPage--;
-            if(curPage < 0) curPage = 0;
-            else switchFragment();
-        });
-
         fragmentTransaction.commit();
     }
+
+    void nextPage() {
+        curPage++;
+        if(curPage >= pageFragments.size()) {
+            startActivity(new Intent(OnboardingActivity.this, LoginActivity.class));
+            finish();
+        }
+        else switchFragment();
+    }
+
+    void prevPage() {
+        curPage--;
+        if(curPage < 0) curPage = 0;
+        else switchFragment();
+    }
+
 }
